0b88e6e446c012f50ccdc29b65bc7d5f872ab2a2
Bnaya Eshet <3382669+bnayae@users.noreply.github.com>
2023-06-25 15:38:51 +0300

    feat: telemetry

--- templates/Skeleton/Skeleton.Service/Extensions/OpenTelemetryExtensions.cs
+++ templates/Skeleton/Skeleton.Service/Extensions/OpenTelemetryExtensions.cs
@@ -1,9 +1,12 @@
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Trace;
 
+// Configuration: https://medium.com/@gparlakov/the-confusion-of-asp-net-configuration-with-environment-variables-c06c545ef732
 // see:
 //  https://opentelemetry.io/docs/instrumentation/net/getting-started/
+//  https://opentelemetry.io/docs/demo/services/cart/
 //  https://github.com/open-telemetry/opentelemetry-dotnet/blob/main/src/OpenTelemetry.Exporter.Jaeger/README.md#environment-variables
+//  https://opentelemetry.io/docs/demo/docker-deployment/
 
 namespace Skeleton;
 
@@ -31,17 +34,19 @@ internal static class OpenTelemetryExtensions
                 .WithEventSourcingTracing(environment,
                         cfg =>
                         {
-                            cfg.AddAspNetCoreInstrumentation(m =>
-                            {
-                                m.Filter = TraceFilter;
-                                m.RecordException = true;
-                                m.EnableGrpcAspNetCoreSupport = true;
-                            })
+                            cfg
+                                .AddAspNetCoreInstrumentation(m =>
+                                {
+                                    m.Filter = TraceFilter;
+                                    m.RecordException = true;
+                                    m.EnableGrpcAspNetCoreSupport = true;
+                                })
                                 .AddHttpClientInstrumentation(m =>
                                 {
                                     // m.Enrich
                                     m.RecordException = true;
                                 })
+                                .AddGrpcClientInstrumentation()
                                 .AddOtlpExporter();
                             if (environment.IsDevelopment())
                                 cfg.AddConsoleExporter();
